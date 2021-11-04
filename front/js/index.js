d368425ce35e1461a07ce0c990a6533b61cdbb8f
Marie <marie.durrmeyer@gmail.com>
2021-11-04 12:06:07 +0100

    installation

---
+++ front/js/index.js
@@ -0,0 +1,73 @@
+/**
+ * Gère l'affichage des produits 
+ */
+
+items()
+
+ async function items() {
+  const products = await getProducts()
+
+  for (product of products){ 
+  displayProduct(product)
+  }
+}
+function getProducts(){ 
+return fetch("http://localhost:3000/api/products")
+  .then((response) => {
+    return response.json();
+  })
+  .then((products) => {
+    return products
+  })
+  .catch((error) => {
+    alert(error)
+  })
+}
+/**
+ * afficher les produits dans la page d'accueil
+ */
+
+function displayProduct(product) {
+  document.getElementById("items").innerHTML += ` <article>
+              <img src="${product.imageUrl}" alt="${product.atlTxt}">
+              <h3 class="productName">"${product.name}"</h3>
+              <p class="productDescription">"${product.description}"</p>
+            </article>       
+               `
+  
+}
+
+/**let productData = [];
+
+const fetchProduct = async () => {
+    await fetch("http://localhost:3000/api/products")
+    .then((response) => response.json())
+    .then ((json) => { productData = json
+  });
+    
+};
+console.log(productData)
+/**
+ * afficher les produits dans la page d'accueil
+ */
+
+ /** const productDisplay = async () => {
+    await fetchProduct();
+    
+    document.querySelector(".items").innerHTML += ` <article>
+              <img src="${productData.imageUrl}" alt="${productData.atlTxt}">
+              <h3 class="productName">"${productData.name}"</h3>
+              <p class="productDescription">"${productData.description}"</p>
+            </article>
+          
+   `
+  
+};
+
+fetchProduct();*/
+
+
+ 
+         
+            
+ 
