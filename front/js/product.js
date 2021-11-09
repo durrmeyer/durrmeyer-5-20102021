946cb2ab99ea8050757f84483b5a698c9e777cb0
Marie <marie.durrmeyer@gmail.com>
2021-11-09 14:47:37 +0100

    banniere

--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,4 @@
-// Récuprération de la requête dans l'URL//
+// Récupération de la requête dans l'URL//
 const queryString_url_id = window.location.search;
 console.log(queryString_url_id);
 
@@ -8,14 +8,9 @@ const _id = queryString_url_id.slice(1);
 console.log(_id);
 
 //affiche le produit selectionné//
-console.log(products);
+
 
 const idProduct = products.find((element) => element._id === _id);
 console.log(idProduct);
 
 //
-
-function displayArticle(product) {
-    Document.querySelector("item").innerHTML = ``
-
-};
