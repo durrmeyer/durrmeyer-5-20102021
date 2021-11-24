53f827dd192ef5f5b2c8e36288b906c4f85ee74c
Marie <marie.durrmeyer@gmail.com>
2021-11-24 14:11:21 +0100

    localstorage

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,6 +1,15 @@
-async function getProduct() {
-  let response = await fetch(`http://localhost:3000/api/products/order/`);
-  let data = await response.json();
-  return data;
-  console.log(data);
-}
+console.log("oh");
+
+//---------------------------------- enregistrement dans le local Storage----------------------------
+const panier = localStorage.getItem("optionProduct");
+
+console.log(panier);
+/**
+ * affiche le produit dans la page produit
+ */
+const cart = document.querySelector("#cart__items");
+console.log(cart);
+getProduct(id).then((id) => {}
+
+
+//------------------------------------ choix de la couleur----------------------------------------------//
--- front/js/product.js
+++ front/js/product.js
@@ -1,7 +1,7 @@
 // Récupération de la requête dans l'URL//
-const SearchParams = window.location.search;
+const searchParams = window.location.search;
 
-const urlSearchParams = new URLSearchParams(SearchParams);
+const urlSearchParams = new URLSearchParams(searchParams);
 const id = urlSearchParams.get("id");
 
 //affiche le produit selectionné//
@@ -23,6 +23,8 @@ getProduct(id).then((product) => {
   document.querySelector("#price").innerText = product.price;
   document.querySelector("#description").innerText = product.description;
 
+  //------------------------------------ choix de la couleur----------------------------------------------//
+
   const selectColors = document.querySelector("#colors");
   for (const color of product.colors) {
     const option = document.createElement("option");
@@ -31,12 +33,40 @@ getProduct(id).then((product) => {
     selectColors.appendChild(option);
   }
 });
-/* Ajouter un article dans le panier */
+//-------------------------------------- choix de la quantité----------------------------------------------//
+const optionQuantite = document.querySelector("#quantity");
+const optionColor = document.querySelector("#colors");
 
+//---------------------------------------- Gestion pour l'envoie au panier------------------------------------//
 const button_envoyer = document.querySelector("#addToCart");
-const selectColors = document.querySelector("#colors");
+button_envoyer.addEventListener("click", (event) => {
+  event.preventDefault();
+  // récuperer les valeurs du produit//
 
-button_envoyer.addEventListener("click", function (event) {
-  event.preventDefault("");
-  document.getElementById("price").innerText = ++parentClicks + "";
+  const optionProduct = {
+    imageUrl: document.querySelector("#imageUrl").innerHTML,
+    title: document.querySelector("#title").innerText,
+    price: document.querySelector("#price").innerText,
+    description: document.querySelector("#description").innerText,
+    color: optionColor.value,
+    quantity: optionQuantite.value,
+  };
+
+  //-----------------------------------redirection--------------------------------------------------------//
+  //--------------------------------------message---------------------------------------------------------//
+
+  //---------------------------------- enregistrement dans le local Storage-------------------------------//
+  let panier = JSON.parse(localStorage.getItem(optionProduct));
+
+  const articlePanier = () => {
+    panier.push(optionProduct);
+    localStorage.setItem([id, optionColor.value], JSON.stringify(panier));
+  };
+  if (panier) {
+    articlePanier();
+  } else {
+    panier = [id];
+    articlePanier();
+  }
+  console.log(panier);
 });
